0c4e271a66e7239e825200d1bf47aada2af337c2
Andrea Ruzittu <ruzittu.andrea97@gmail.com>
2023-12-16 17:52:58 +0100

    upDates

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,45 +13,110 @@ document.addEventListener('DOMContentLoaded', function () {
         var sunIcon = this.querySelector('.fa-sun');
         moonIcon.style.display = moonIcon.style.display === 'none' ? 'block' : 'none';
         sunIcon.style.display = sunIcon.style.display === 'none' ? 'block' : 'none';
+        closeNavbar();
     });
 });
 
-  
-  // Chiusura NavBar
-  document.addEventListener('click', function(event) {
-    // Controlla se il click è sul selettore di lingua
-    let isLanguageSwitcher = event.target.id === 'language-switcher';
-
-    // Il resto del codice rimane lo stesso
+  // Funzione per chiudere la navbar
+function closeNavbar() {
+  let navbarCollapse = document.querySelector('.navbar-collapse');
+  if (navbarCollapse.classList.contains('show')) {
+    document.querySelector('.navbar-toggler').click();
+  }
+}
+ // Chiusura NavBar
+document.addEventListener('click', function(event) {
     let insideNavbar = document.querySelector('.navbar').contains(event.target);
     let isNavbarToggler = event.target.classList.contains('navbar-toggler') || event.target.closest('.navbar-toggler') != null;
     let navbarCollapse = document.querySelector('.navbar-collapse');
     let isNavbarOpen = navbarCollapse.classList.contains('show');
 
-    // Aggiungi la condizione per il selettore di lingua qui
-    if (isNavbarOpen && !insideNavbar && !isLanguageSwitcher) {
-        document.querySelector('.navbar-toggler').click();
+    // Chiude la navbar se è aperta e se il click è avvenuto fuori dalla navbar e non sul toggler
+    if (isNavbarOpen && !insideNavbar && !isNavbarToggler) {
+      closeNavbar();
     }
 });
 
+// Chiudi la navbar al cambio della lingua
+document.getElementById('language-switcher').addEventListener('change', function() {
+  var language = this.value;
+  changeLanguage(language);
+  closeNavbar();
+});
+// Chiudi la navbar alla selezione di un link
+document.querySelectorAll('.navbar-nav .nav-link').forEach(link => {
+  link.addEventListener('click', closeNavbar);
+});
 
-  
-  
 
-    // Cambio lingua 
+    
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Imposta la lingua italiana come predefinita al caricamento della pagina
+    changeLanguage('it');
+
+    // Imposta il selettore di lingua su italiano
+    document.getElementById('language-switcher').value = 'it';
+
+    // Listener per il cambio di lingua tramite il selettore
     document.getElementById('language-switcher').addEventListener('change', function() {
-        var language = this.value;
-        console.log("Lingua selezionata:", language);
+      var language = this.value;
+      changeLanguage(language);
     });
+});
+
+function changeLanguage(language) {
+    for (const key in translations[language]) {
+      const element = document.getElementById(key);
+      if (element) {
+        element.textContent = translations[language][key];
+      }
+    }
+    // Aggiorna il selettore di lingua
+    document.getElementById('language-switcher').value = language;
+}
+      
+    const translations = {
+        it: {
+          'title': 'Ciao, sono Andrea Ruzittu',
+          'aboutMe': `Ciao, sono Andrea. Dopo una laurea in Economia, ho iniziato a lavorare nell'amministrazione di una catena alberghiera, acquisendo competenze in gestione e servizio clienti. La mia passione per la tecnologia mi ha poi portato a frequentare un bootcamp di programmazione, dove mi sono specializzato in sviluppo web Full Stack. Oggi, integro la mia comprensione del business con le competenze tecniche per sviluppare soluzioni web innovative, sempre con un occhio al continuo apprendimento e alla collaborazione.`,
+          'fullStack': 'SVILUPPATORE FULL-STACK',
+          'ab':`Su di me...`,
+          'progetti':`Progetti`,
+          'contattami':`Contattami`,
+          'skills':`Competenze`,
+          'formMail':`Indirizzo Email`,
+          'formMessage':`Messaggio`,
+          'nomme':`Nome`,
+          'formSend':`Inviami un messaggio`,
+          'view':`Visualsji`
+          
+        },
+        en: {
+          'title': `Hello, i'm Andrea Ruzittu`,
+          'aboutMe': `Hi, I'm Andrea. After a degree in Economics, I started working in the administration of a hotel chain, acquiring skills in management and customer service. My passion for technology then led me to attend a programming bootcamp, where I specialized in Full Stack web development. Today, I integrate my understanding of business with the technical skills to develop innovative web solutions, always with an eye on continuous learning and collaboration.`,
+          'fullStack': 'FULL-STACK DEVELOPER',
+          'ab':`about me...`,
+          'progetti':`Projects`,
+          'contattami':`Contact me`,
+          'skills':`Skills`,
+          'formMail':`Email address`,
+          'formMessage':`Message`,
+          'nomme':`Name`,
+          'formSend':`Send me a message`
+
+        }
+      };
+
     
 // Anno aggiornato in tempo reale
     var anno= document.getElementById("anno");
     var data = new Date().getFullYear();
     anno.innerHTML = data
 
-    // Elimina dati del form 1 secodo dopo l'invio
-    document.getElementById('contactForm').addEventListener('submit', function() {
-        setTimeout(function() {
-            document.getElementById('contactForm').reset();
-        }, 1000); 
-    });
+    document.getElementById('contactForm').addEventListener('submit', function(e) {
+      setTimeout(function() {
+          document.getElementById('contactForm').reset();
+      }, 1000); 
+  });
+  
